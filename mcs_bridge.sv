d13cb51784833f59882ba1b71f009cca26ab3571
Reshad Haque <reshadulhaque1@gmail.com>
2024-12-05 19:07:14 -0800

    FPRO Bridge Module added

--- mcs_bridge.sv
+++ mcs_bridge.sv
@@ -1,11 +1,11 @@
-module mcs_bridge #(parameter BRG_BASE = 30'hc0000_0000)
+module mcs_bridge #(parameter BRG_BASE = 32'h4000_0000)
 (
     //Nios II processor Avalon to External Bus Bridge
-    input logic rw,
-    input logic [1:0] io_byte_enable,
-    input logic [11:0] io_address,
-    input logic [15:0] io_write_data,
-    input logic [15:0] io_read_data,
+    input logic io_rw,
+    input logic [3:0] io_byte_enable,
+    input logic [29:0] io_address,
+    input logic [31:0] io_write_data,
+    input logic [31:0] io_read_data,
 
     //FPRO Bus
     output logic fp_video_cs,
@@ -17,6 +17,24 @@ module mcs_bridge #(parameter BRG_BASE = 30'hc0000_0000)
     input logic [31:0] fp_rd_data
 );
 
+    //Signal declaration
+    logic mcs_bridge_en;
+    logic [29:0] word_address;
 
+    //Body
+    //Address translation and decoding
+    assign word_address = io_address;
+    assign mcs_bridge_en = (io_address[29:22] == BRG_BASE[31:24]);
+    assign fp_video_cs = (mcs_bridge_en && io_address[21] == 1);
+    assign fp_mmio_cs = (mcs_bridge_en && io_address[21] == 0);
+    assign fp_addr = word_address[20:0];
 
+    //control line conversions
+    assign fp_wr = io_rw;
+    assign fp_rd = ~io_rw;
+
+    //data line conversions
+    assign fp_wr_data = io_write_data;
+    assign fp_rd_dat = io_read_data;
+    
 endmodule
