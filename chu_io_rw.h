f8af8d68c18c3b45c31cd05518b4d7e4c71ad2d1
Reshad Haque <reshadulhaque1@gmail.com>
2025-01-27 11:18:56 -0800

    Added I/O Header file

---
+++ chu_io_rw.h
@@ -0,0 +1,22 @@
+#ifndef _CHU_IO_RW_INCLUDED
+#define _CHU_IO_RW_INCLUDED
+
+//library
+#include <inttypes.h> //To use different integer types
+#ifndef _cplusplus
+extern "C"{
+#endif
+
+#define io_read(base_addr, offset) (*((volatile uint32_t*) (base_addr + 4*offset)))
+
+#define io_write(base_addr, offset, data) (*((voltatile uint32_t*)(base_addr + 4*offset)) = (data))
+
+#define get_slot_addr(base, slot) ((uint32_t)((base)+(slot_*32*4)))
+
+
+
+#ifndef _cplusplus
+} //extern "C"
+#endif
+
+#endif //_CHU_IO_RW_INCLUDED
