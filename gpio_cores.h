d1d92bc4e6a6f1249f25b20613954f4e642cf839
Reshad Haque <reshadulhaque1@gmail.com>
2025-01-28 11:49:25 -0800

    Added header for gpo file

---
+++ gpio_cores.h
@@ -0,0 +1,17 @@
+class GpoCore{
+    public:
+    //register map
+    enum{
+        DATA_REG = 0    //Data register
+    };
+
+    GpoCore(uin32_t core_base_addr); //Constructor
+    ~GpoCore();                      //Destructor
+
+    void write(uint32_t data);      //Write a 32 bit word
+    void write(int bit_value, int bit_pos);  //Write 1 bit (overloaded)
+
+    private:
+        uint32_t base_addr;
+        uint32_t wr_data;          //Same as the GPO core data reg
+};
